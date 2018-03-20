6703583bdbb1c70aadb093672a56f4efe715d438
demidov13 <vitaliydemidov13@gmail.com>
2018-03-20 22:17:33 +0200

    задебажил перебор статей на главной странице

--- views/home.php
+++ views/home.php
@@ -1,5 +1,7 @@
  <h1 class="mt-5">Домашняя страница</h1>
+ <?php if(!empty($rticles)): ?>
  <?php foreach($articles as $article): ?>
     <h3><a href="/index.php?action=edit&id=<?=$article['id']?>"><?=$article['title']?></a></h3>
     <p><?=$article['content']?></p>
     <?php endforeach ?>
+ <?php endif ?>
