e11f13da2f8083f488e4ebd4c7d4077170ae8118
demidov13 <vitaliydemidov13@gmail.com>
2018-05-19 01:56:29 +0300

    удалил лишний return

--- classes/SqlProducts.php
+++ classes/SqlProducts.php
@@ -6,7 +6,6 @@ class SqlProducts extends Sql
 	public function update($newArr, $oldArr, $id)
 	{
 		$update = array_diff_assoc($newArr, $oldArr);
-		return $update;
 		foreach ($update as $name => $value) {
 			if(!empty($value)){
 				mysqli_query($this->mysqli, "UPDATE products SET $name = $value WHERE id = $id");
